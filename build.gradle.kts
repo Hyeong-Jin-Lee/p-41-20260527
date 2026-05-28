59c58526c4b3313498f87967dc8bd03fb3b21ef6
BroJin <gudwls2777@naver.com>
2026-05-28 11:39:42 +0900

    redis

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,12 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    //레디스 의존성
+    implementation("com.github.codemonstur:embedded-redis:1.4.3")
+
+    //스프링 캐시 의존성
+    implementation("org.springframework.boot:spring-boot-starter-cache")
 }
 
 kotlin {
---
+++ src/main/kotlin/com/back/demo1/domain/post/post/Service/PostService.kt
@@ -0,0 +1,19 @@
+package com.back.demo1.domain.post.post.Service
+
+import com.back.demo1.domain.post.post.entity.Post
+import com.back.demo1.domain.post.post.repository.PostRepository
+import org.springframework.stereotype.Service
+
+
+@Service
+class PostService(
+    private val postRepository: PostRepository
+) {
+    fun count() = postRepository.count()
+
+    fun write(title: String, content: String): Post {
+        return postRepository.save(Post(title = title, content = content))
+    }
+
+    fun findAll() = postRepository.findAll()
+}
